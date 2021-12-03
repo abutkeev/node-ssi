f6bd8b606535cd94741778d3406daa29f12ef11d
Alexey Butkeev <git@abutkeev.ru>
2021-12-03 11:39:47 +0300

    return empty string if directive is unsupported

---
+++ test/testUnknown.js
@@ -0,0 +1,15 @@
+
+var assert = require("assert");
+var ssi = require("../index.js");
+
+describe("unsupported directives", function() {
+	var config = "<!--#config timefmt=\"%Y\" -->";
+
+	it("it should output nothing when nothing is found", function() {
+		var parser = new ssi("", "", "");
+		var results = parser.parse("", config);
+		
+		assert.equal("", results.contents);
+	});
+});
+
